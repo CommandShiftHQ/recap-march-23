b7142c10b8b1f2a2d959d7ac0cb6d5231f6e2681
Rabbia Tul-Zohra <rtulzohr@jaguarlandrover.com>
2023-08-30 20:08:57 +0100

    removed unused code

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -2,10 +2,9 @@ import "../styles/artist.css";
 
 const Artist = ({ image, name }) => {
   return (
-    <div className="card">
-      <img src={image} className="img" alt="" />
-      <div className="h3">{name}</div>
-      <div>Hi!</div>
+    <div className='card'>
+      <img src={image} className='img' alt='' />
+      <div className='h3'>{name}</div>
     </div>
   );
 };
